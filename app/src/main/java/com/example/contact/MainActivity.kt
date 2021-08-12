f69b739b909d9396c7be8c8b8eb97358ee9ceb69
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-08-12 16:13:22 -0300

    finalizando app com shared preferences

--- app/src/main/java/com/example/contact/MainActivity.kt
+++ app/src/main/java/com/example/contact/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.contact
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -10,10 +12,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.edit
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contact.ContactDetailActivity.Companion.EXTRA_CONTACT
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity(), ClickItemContactListener {
 
@@ -28,8 +33,34 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
         setContentView(R.layout.drawer_menu)
 
         initDrawer()
+        fetchListContact()
         bindView()
-        updateList()
+    }
+
+    private fun fetchListContact() { // busca
+        val list = arrayListOf(
+            Contact(
+                "Mateus Paulart",
+                "(51) 99938-1964",
+            ),
+            Contact(
+                "Matue",
+                "(85) 99999-2344",
+            ),
+            Contact(
+                "lionel messi",
+                "(11) 98800-0990",
+            ),
+        )
+        getInstanceSharedPreferences().edit {
+            val json = Gson().toJson(list)
+            putString("contacts", json)
+            commit()
+        }
+    }
+
+    private fun getInstanceSharedPreferences(): SharedPreferences {
+        return getSharedPreferences("com.exemple.contact.PREFERENCES", Context.MODE_PRIVATE)
     }
 
     private fun initDrawer(){
@@ -47,25 +78,18 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
     private fun bindView(){
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact> { // converte string para objeto de classe
+        val list = getInstanceSharedPreferences().getString("contacts", "[]")
+        val turnsType= object : TypeToken<List<Contact>>() {}.type
+        return Gson().fromJson(list, turnsType)
     }
 
     private fun updateList() {
-        adapter.updateList(
-            arrayListOf(
-                Contact(
-                    "Mateus Paulart",
-                    "(51) 99938-1964",
-                ),
-                Contact(
-                    "Matue",
-                    "(85) 99999-2344",
-                ),
-                Contact(
-                    "lionel messi",
-                    "(11) 98800-0990",
-                ),
-            )
-        )
+        val list = getListContacts()
+        adapter.updateList(list)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
